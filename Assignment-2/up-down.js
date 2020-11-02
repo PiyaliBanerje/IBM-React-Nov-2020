e5a052ae62ecc343fc3c2dee995c2c981f849bf9
PiyaliBanerje <73816968+PiyaliBanerje@users.noreply.github.com>
2020-11-02 15:25:33 +0530

    Create up-down.js

---
+++ Assignment-2/up-down.js
@@ -0,0 +1,10 @@
+(function myapp() {var spinner = {
+    i : 0,
+    up : function() {
+        return this.i=this.i+1;
+    },
+    down : function() {
+        return this.i=this.i-1;
+    }
+}
+})();
